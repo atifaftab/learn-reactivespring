d9acf696cabdbeae7c6054b9508477386f73fcee
Atif <iamatif96@gmail.com>
2023-10-01 17:59:27 +0400

    error removed

--- src/main/java/com/learnreactivespring/fluxAndMonoPlayground/FluxAndMonoTest.java
+++ src/main/java/com/learnreactivespring/fluxAndMonoPlayground/FluxAndMonoTest.java
@@ -9,9 +9,11 @@ public class FluxAndMonoTest {
     @Test
     public void fluxTest() {
         Flux<String> stringFlux = Flux.just("Spring", "Spring Boot")
-                .concatWith(Flux.error(new RuntimeException("Run time exception")))
+//                .concatWith(Flux.error(new RuntimeException("Run time exception")))
+                .concatWith(Flux.just("Reactive java"))
                 .log();
         stringFlux.subscribe(System.out::println,
-                System.out::println);
+                System.out::println,
+                () -> System.out.println("Completed !"));
     }
 }
