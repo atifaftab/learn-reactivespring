a1be4dc537b3fb0a155216dba6a2dcb96e5a44ae
Atif <iamatif96@gmail.com>
2023-10-03 18:57:20 +0400

    mono added

--- src/main/java/com/learnreactivespring/fluxAndMonoPlayground/FluxAndMonoTest.java
+++ src/main/java/com/learnreactivespring/fluxAndMonoPlayground/FluxAndMonoTest.java
@@ -3,6 +3,8 @@ package com.learnreactivespring.fluxAndMonoPlayground;
 
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
 
@@ -15,5 +17,36 @@ public class FluxAndMonoTest {
         stringFlux.subscribe(System.out::println,
                 System.out::println,
                 () -> System.out.println("Completed !"));
+    }
+
+    @Test
+    public void fluxTestElements_WithoutError() {
+        Flux<String> stringFlux = Flux.just("Spring", "Spring Boot")
+                .log();
+        StepVerifier.create(stringFlux)
+                .expectNext("Spring")
+                .expectNext("Spring Boot")
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxTestElements_WithError() {
+        Flux<String> stringFlux = Flux.just("Spring", "Spring Boot")
+                .concatWith(Flux.error(new RuntimeException("Runtime error")))
+                .log();
+        StepVerifier.create(stringFlux)
+                .expectNext("Spring")
+                .expectNext("Spring Boot")
+//                .expectError(RuntimeException.class)
+                .expectErrorMessage("Runtime error")
+                .verify();
+    }
+
+    @Test
+    public void stringMono() {
+        Mono<String> stringMono = Mono.just("Spring");
+        StepVerifier.create(stringMono.log())
+                .expectNext("Spring")
+                .verifyComplete();
     }
 }
